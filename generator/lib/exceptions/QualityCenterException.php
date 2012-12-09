093a5976af7f28f92b171cbe74f87d380c9d3326
johnathan.kanarek <johnathan.kanarek@gmail.com>
2012-12-09 12:44:07 +0200

    Enable empty message and code

--- generator/lib/exceptions/QualityCenterException.php
+++ generator/lib/exceptions/QualityCenterException.php
@@ -5,7 +5,7 @@
  */
 class QualityCenterException extends Exception
 {
-	public function __construct ($message, $code, $previous = null)
+	public function __construct ($message = null, $code = null, $previous = null)
 	{
 		parent::__construct($message, null, $previous);
 		$this->code = $code;
