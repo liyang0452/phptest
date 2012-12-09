468031d9e4f0e702f28ded189fb3375a49924b2b
johnathan.kanarek <johnathan.kanarek@gmail.com>
2012-12-09 12:59:40 +0200

    Support login exceptions

---
+++ generator/lib/exceptions/QualityCenterLoginException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package External
+ * @subpackage qc.services
+ */
+require_once __DIR__ . '/QualityCenterException.php';
+
+/**
+ * @package External
+ * @subpackage qc.exception
+ */
+class QualityCenterLoginException extends QualityCenterException
+{
+}
